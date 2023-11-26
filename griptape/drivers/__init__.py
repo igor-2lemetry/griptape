d0060c439954ac93a45bf26a34d7edd3fd07eca5
Andrew French <andrew@afren.ch>
2023-11-25 19:20:53 -0800

    Initial support for image-to-image workloads

--- griptape/drivers/__init__.py
+++ griptape/drivers/__init__.py
@@ -1,60 +1,58 @@
-from .prompt.base_prompt_driver import BasePromptDriver
-from .prompt.openai_chat_prompt_driver import OpenAiChatPromptDriver
-from .prompt.openai_completion_prompt_driver import OpenAiCompletionPromptDriver
-from .prompt.azure_openai_chat_prompt_driver import AzureOpenAiChatPromptDriver
-from .prompt.azure_openai_completion_prompt_driver import AzureOpenAiCompletionPromptDriver
-from .prompt.cohere_prompt_driver import CoherePromptDriver
-from .prompt.hugging_face_pipeline_prompt_driver import HuggingFacePipelinePromptDriver
-from .prompt.hugging_face_hub_prompt_driver import HuggingFaceHubPromptDriver
-from .prompt.anthropic_prompt_driver import AnthropicPromptDriver
-from .prompt.amazon_sagemaker_prompt_driver import AmazonSageMakerPromptDriver
-from .prompt.amazon_bedrock_prompt_driver import AmazonBedrockPromptDriver
-from .prompt.base_multi_model_prompt_driver import BaseMultiModelPromptDriver
-
-from .memory.conversation.base_conversation_memory_driver import BaseConversationMemoryDriver
-from .memory.conversation.local_conversation_memory_driver import LocalConversationMemoryDriver
-from .memory.conversation.amazon_dynamodb_conversation_memory_driver import AmazonDynamoDbConversationMemoryDriver
-
-from .embedding.base_embedding_driver import BaseEmbeddingDriver
-from .embedding.openai_embedding_driver import OpenAiEmbeddingDriver
 from .embedding.azure_openai_embedding_driver import AzureOpenAiEmbeddingDriver
+from .embedding.base_embedding_driver import BaseEmbeddingDriver
 from .embedding.bedrock_titan_embedding_driver import BedrockTitanEmbeddingDriver
-
-from .vector.base_vector_store_driver import BaseVectorStoreDriver
-from .vector.local_vector_store_driver import LocalVectorStoreDriver
-from .vector.pinecone_vector_store_driver import PineconeVectorStoreDriver
-from .vector.marqo_vector_store_driver import MarqoVectorStoreDriver
-from .vector.mongodb_vector_store_driver import MongoDbAtlasVectorStoreDriver
-from .vector.redis_vector_store_driver import RedisVectorStoreDriver
-from .vector.opensearch_vector_store_driver import OpenSearchVectorStoreDriver
-from .vector.amazon_opensearch_vector_store_driver import AmazonOpenSearchVectorStoreDriver
-from .vector.pgvector_vector_store_driver import PgVectorVectorStoreDriver
-
-from .sql.base_sql_driver import BaseSqlDriver
-from .sql.amazon_redshift_sql_driver import AmazonRedshiftSqlDriver
-from .sql.snowflake_sql_driver import SnowflakeSqlDriver
-from .sql.sql_driver import SqlDriver
-
-from .prompt_model.base_prompt_model_driver import BasePromptModelDriver
-from .prompt_model.sagemaker_llama_prompt_model_driver import SageMakerLlamaPromptModelDriver
-from .prompt_model.sagemaker_falcon_prompt_model_driver import SageMakerFalconPromptModelDriver
-from .prompt_model.bedrock_titan_prompt_model_driver import BedrockTitanPromptModelDriver
-from .prompt_model.bedrock_claude_prompt_model_driver import BedrockClaudePromptModelDriver
-from .prompt_model.bedrock_jurassic_prompt_model_driver import BedrockJurassicPromptModelDriver
-
-from .image_generation.base_image_generation_driver import BaseImageGenerationDriver
-from .image_generation.base_multi_model_image_generation_driver import BaseMultiModelImageGenerationDriver
-from .image_generation.openai_dalle_image_generation_driver import OpenAiDalleImageGenerationDriver
-from .image_generation.leonardo_image_generation_driver import LeonardoImageGenerationDriver
-from .image_generation.amazon_bedrock_image_generation_driver import AmazonBedrockImageGenerationDriver
-
-from .image_generation_model.base_image_generation_model_driver import BaseImageGenerationModelDriver
-from .image_generation_model.amazon_bedrock_titan_image_generation_model_driver import (
-    AmazonBedrockTitanImageGenerationModelDriver,
+from .embedding.openai_embedding_driver import OpenAiEmbeddingDriver
+from .image.amazon_bedrock_image_generation_driver import AmazonBedrockImageGenerationDriver
+from .image.amazon_bedrock_stable_diffusion_image_generation_driver import (
+    AmazonBedrockStableDiffusionImageGenerationDriver,
 )
+from .image.base_image_driver import BaseImageDriver
+from .image.base_image_generation_driver import BaseImageGenerationDriver
+from .image.base_image_modification_driver import BaseImageModificationDriver
+from .image.base_multi_model_image_generation_driver import BaseMultiModelImageGenerationDriver
+from .image.leonardo_image_generation_driver import LeonardoImageGenerationDriver
+from .image.openai_dalle_image_generation_driver import OpenAiDalleImageGenerationDriver
 from .image_generation_model.amazon_bedrock_stable_diffusion_image_generation_model_driver import (
     AmazonBedrockStableDiffusionImageGenerationModelDriver,
 )
+from .image_generation_model.amazon_bedrock_titan_image_generation_model_driver import (
+    AmazonBedrockTitanImageGenerationModelDriver,
+)
+from .image_generation_model.base_image_generation_model_driver import BaseImageGenerationModelDriver
+from .memory.conversation.amazon_dynamodb_conversation_memory_driver import AmazonDynamoDbConversationMemoryDriver
+from .memory.conversation.base_conversation_memory_driver import BaseConversationMemoryDriver
+from .memory.conversation.local_conversation_memory_driver import LocalConversationMemoryDriver
+from .prompt.amazon_bedrock_prompt_driver import AmazonBedrockPromptDriver
+from .prompt.amazon_sagemaker_prompt_driver import AmazonSageMakerPromptDriver
+from .prompt.anthropic_prompt_driver import AnthropicPromptDriver
+from .prompt.azure_openai_chat_prompt_driver import AzureOpenAiChatPromptDriver
+from .prompt.azure_openai_completion_prompt_driver import AzureOpenAiCompletionPromptDriver
+from .prompt.base_multi_model_prompt_driver import BaseMultiModelPromptDriver
+from .prompt.base_prompt_driver import BasePromptDriver
+from .prompt.cohere_prompt_driver import CoherePromptDriver
+from .prompt.hugging_face_hub_prompt_driver import HuggingFaceHubPromptDriver
+from .prompt.hugging_face_pipeline_prompt_driver import HuggingFacePipelinePromptDriver
+from .prompt.openai_chat_prompt_driver import OpenAiChatPromptDriver
+from .prompt.openai_completion_prompt_driver import OpenAiCompletionPromptDriver
+from .prompt_model.base_prompt_model_driver import BasePromptModelDriver
+from .prompt_model.bedrock_claude_prompt_model_driver import BedrockClaudePromptModelDriver
+from .prompt_model.bedrock_jurassic_prompt_model_driver import BedrockJurassicPromptModelDriver
+from .prompt_model.bedrock_titan_prompt_model_driver import BedrockTitanPromptModelDriver
+from .prompt_model.sagemaker_falcon_prompt_model_driver import SageMakerFalconPromptModelDriver
+from .prompt_model.sagemaker_llama_prompt_model_driver import SageMakerLlamaPromptModelDriver
+from .sql.amazon_redshift_sql_driver import AmazonRedshiftSqlDriver
+from .sql.base_sql_driver import BaseSqlDriver
+from .sql.snowflake_sql_driver import SnowflakeSqlDriver
+from .sql.sql_driver import SqlDriver
+from .vector.amazon_opensearch_vector_store_driver import AmazonOpenSearchVectorStoreDriver
+from .vector.base_vector_store_driver import BaseVectorStoreDriver
+from .vector.local_vector_store_driver import LocalVectorStoreDriver
+from .vector.marqo_vector_store_driver import MarqoVectorStoreDriver
+from .vector.mongodb_vector_store_driver import MongoDbAtlasVectorStoreDriver
+from .vector.opensearch_vector_store_driver import OpenSearchVectorStoreDriver
+from .vector.pgvector_vector_store_driver import PgVectorVectorStoreDriver
+from .vector.pinecone_vector_store_driver import PineconeVectorStoreDriver
+from .vector.redis_vector_store_driver import RedisVectorStoreDriver
 
 __all__ = [
     "BasePromptDriver",
@@ -95,8 +93,11 @@ __all__ = [
     "BedrockTitanPromptModelDriver",
     "BedrockClaudePromptModelDriver",
     "BedrockJurassicPromptModelDriver",
+    "BaseImageDriver",
     "BaseImageGenerationDriver",
     "BaseMultiModelImageGenerationDriver",
+    "BaseImageModificationDriver",
+    "BaseImageInpaintingDriver",
     "OpenAiDalleImageGenerationDriver",
     "LeonardoImageGenerationDriver",
     "AmazonBedrockImageGenerationDriver",
