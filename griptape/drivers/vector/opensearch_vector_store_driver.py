03c76bbe4f8c575804325dafa1cf6562c7fccf2f
Igor Morozov <igor@2lemetry.ru>
2023-12-12 17:36:22 +0300

    minor changes in prompt, logs for vector db response

--- griptape/drivers/vector/opensearch_vector_store_driver.py
+++ griptape/drivers/vector/opensearch_vector_store_driver.py
@@ -150,6 +150,9 @@ class OpenSearchVectorStoreDriver(BaseVectorStoreDriver):
 
         response = self.client.search(index=self.index_name, body=query_body)
 
+        print(">>>>> Search Results")
+        print(response)
+
         return [
             BaseVectorStoreDriver.QueryResult(
                 id=hit["_id"],
