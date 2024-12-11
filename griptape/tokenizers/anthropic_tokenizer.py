a9a1a170b6ec090a398f7477b4296b214b335701
Igor Morozov <igor@2lemetry.ru>
2024-12-11 16:31:26 +0300

    use anthropic_tokenizer from the last griptape version

--- griptape/tokenizers/anthropic_tokenizer.py
+++ griptape/tokenizers/anthropic_tokenizer.py
@@ -1,31 +1,36 @@
 from __future__ import annotations
-from attr import define, field, Factory
-from typing import TYPE_CHECKING, Optional
-from griptape.utils import import_optional_dependency
+
+from typing import TYPE_CHECKING
+
+from attrs import Factory, define, field
+
 from griptape.tokenizers import BaseTokenizer
+from griptape.utils import import_optional_dependency
 
 if TYPE_CHECKING:
     from anthropic import Anthropic
+    from anthropic.types.beta import BetaMessageParam
 
 
-@define(frozen=True)
+@define()
 class AnthropicTokenizer(BaseTokenizer):
-    DEFAULT_MODEL = "claude-2.1"
-    MODEL_PREFIXES_TO_MAX_TOKENS = {"claude-2.1": 200000, "claude": 100000}
+    MODEL_PREFIXES_TO_MAX_INPUT_TOKENS = {"claude-3": 200000, "claude-2.1": 200000, "claude": 100000}
+    MODEL_PREFIXES_TO_MAX_OUTPUT_TOKENS = {"claude": 4096}
 
-    model: str = field(kw_only=True)
-    max_tokens: int = field(kw_only=True, default=Factory(lambda self: self.default_max_tokens(), takes_self=True))
     client: Anthropic = field(
-        default=Factory(lambda: import_optional_dependency("anthropic").Anthropic()), kw_only=True
+        default=Factory(lambda: import_optional_dependency("anthropic").Anthropic()),
+        kw_only=True,
     )
 
-    def default_max_tokens(self) -> int:
-        tokens = next(v for k, v in self.MODEL_PREFIXES_TO_MAX_TOKENS.items() if self.model.startswith(k))
+    def count_tokens(self, text: str | list[BetaMessageParam]) -> int:
+        types = import_optional_dependency("anthropic.types.beta")
 
-        return tokens
+        # TODO: Refactor all Tokenizers to support Prompt Stack as an input.
+        messages = [types.BetaMessageParam(role="user", content=text)] if isinstance(text, str) else text
 
-    def count_tokens(self, text: str | list) -> int:
-        if isinstance(text, str):
-            return self.client.count_tokens(text)
-        else:
-            raise ValueError("Text must be a string.")
+        usage = self.client.beta.messages.count_tokens(
+            model=self.model,
+            messages=messages,
+        )
+
+        return usage.input_tokens
